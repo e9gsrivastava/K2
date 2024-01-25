49d59c6211e3ca6b8eb89dcbbc160440a9de0ac5
e9gsrivastava <153479304+e9gsrivastava@users.noreply.github.com>
2024-01-25 14:36:05 +0530

    Update models.py

--- k2/progress_tracker/models.py
+++ k2/progress_tracker/models.py
@@ -1,6 +1,7 @@
 # progress_report/models.py
 
 from django.db import models
+from django.db.models import Avg
 
 
 class Trainee(models.Model):
@@ -8,6 +9,50 @@ class Trainee(models.Model):
     username = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
 
+    @property
+    def get_progress(self):
+        progress_reports = ProgressReport.objects.filter(trainee=self)
+
+        attendance_percentage = (
+            progress_reports.aggregate(average=Avg("attendance"))["average"] / 100.0
+            if progress_reports
+            else 0
+        )
+        marks_percentage = (
+            progress_reports.aggregate(average=Avg("marks"))["average"] / 100.0
+            if progress_reports
+            else 0
+        )
+        assignment_percentage = (
+            progress_reports.aggregate(average=Avg("assignment"))["average"] / 100.0
+            if progress_reports
+            else 0
+        )
+
+        return (
+            attendance_percentage + marks_percentage + assignment_percentage
+        ) / 3
+    
+    @property
+    def progress(self):
+
+        progress_reports = ProgressReport.objects.filter(trainee=self)
+        percentages = [report.attendance / 100.0 for report in progress_reports]
+
+        return percentages
+    
+    @property
+    def get_marks(self):
+        progress_reports = ProgressReport.objects.filter(trainee=self)
+        marks = [report.marks / 100.0 for report in progress_reports]
+        return marks
+    
+    @property
+    def get_assignment(self):
+        progress_reports = ProgressReport.objects.filter(trainee=self)
+        assignments = [report.assignment / 100.0 for report in progress_reports]
+        return assignments
+    
 
 class ProgressReport(models.Model):
     trainee = models.ForeignKey(Trainee, on_delete=models.CASCADE)
